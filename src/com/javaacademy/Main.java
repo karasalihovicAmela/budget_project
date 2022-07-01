7d7681087c0b9bb8e23a5e9ff043c40e287476b9
Muamer <s.muamer1@gmail.com>
2022-07-01 15:52:31 +0200

    Adding feature for logging in and out of the App

--- src/com/javaacademy/Main.java
+++ src/com/javaacademy/Main.java
@@ -1,5 +1,6 @@
 package com.javaacademy;
 
+import com.javaacademy.entities.User;
 import com.javaacademy.repositories.UserRepository;
 import com.javaacademy.services.CategoryService;
 import com.javaacademy.services.DatabaseService;
@@ -25,28 +26,70 @@ public class Main {
         UserRepository userRepository = new UserRepository();
         userRepository.createUserTable(connection);
 
-        int choice = -1;
+        User loggedInUser = userRepository.getLoggedInUser(connection);
+        while (loggedInUser == null) {
+            int choice = -1;
 
-        while(choice != 0) {
-            System.out.println("Welcome to budgeting app! Please choose: \n0 - Close app, " +
-                    "\n1 - Log into app or \n2 - Register user ");
-            choice = scanner.nextInt();
-            switch (choice) {
-                case 0:
-                    System.out.println("Thanks for visiting us!");
-                    break;
-                case 1:
-                    System.out.println("You choose to log into app! Please follow next steps: ");
-                    userService.login(scanner, connection);
-                    break;
-                case 2:
-                    System.out.println("You choose to register user! Please follow next setps: ");
-                    userService.register(scanner, connection);
-                    break;
-                default:
-                    System.out.println("Wrong choice, please try again: ");
-                    break;
+            while (choice != 0) {
+                System.out.println("\nWelcome to budgeting app! Please choose: " +
+                        "\n0 - Close app, " +
+                        "\n1 - Log into app or " +
+                        "\n2 - Register user ");
+                choice = scanner.nextInt();
+                switch (choice) {
+                    case 0:
+                        System.out.println("Thanks for visiting us!");
+                        System.exit(0);
+                        break;
+                    case 1:
+                        System.out.println("You choose to log into app! Please follow next steps: ");
+                        loggedInUser = userService.login(scanner, connection);
+                        choice = 0;
+                        break;
+                    case 2:
+                        System.out.println("You choose to register user! Please follow next setps: ");
+                        userService.register(scanner, connection);
+                        break;
+                    default:
+                        System.out.println("Wrong choice, please try again: ");
+                        break;
+                }
             }
+
+            while (loggedInUser != null) {
+                int choiceInApp = -1;
+
+                while (choiceInApp != 0) {
+                    System.out.println("\nWelcome to Budgeting App Dashboard! " +
+                            "\nPlease choose: " +
+                            "\n0 - Log out, " +
+                            "\n1 - Add a new bugdet, " +
+                            "\n2 - Add a new transaction, " +
+                            "\n3 - Import transactions from file, " +
+                            "\n4 - Export budget report or" +
+                            "\n5 - Check budget status");
+                    choiceInApp = scanner.nextInt();
+                    switch (choiceInApp) {
+                        case 0:
+                            System.out.println("Logging you out!");
+                            userService.logout(connection);
+                            loggedInUser = null;
+                            break;
+                        case 1:
+                            System.out.println("You choose to log into app! Please follow next steps: ");
+                            loggedInUser = userService.login(scanner, connection);
+                            break;
+                        case 2:
+                            System.out.println("You choose to register user! Please follow next setps: ");
+                            userService.register(scanner, connection);
+                            break;
+                        default:
+                            System.out.println("Wrong choice, please try again: ");
+                            break;
+                    }
+                }
+            }
+
         }
 
         databaseService.closeDatabaseConnection(connection);
